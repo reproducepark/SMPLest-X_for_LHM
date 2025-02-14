89dc1678d0dc81c808154859471221aa18af9144
yinwanqi <wanqi.yin5@gmail.com>
2025-02-14 19:05:18 +0800

    new codebase released

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+conda create -n smplestx python=3.8 -y
+conda activate smplestx
+conda install pytorch==1.12.0 torchvision==0.13.0 torchaudio==0.12.0 cudatoolkit=11.3 -c pytorch -y
+pip install -r requirements.txt
+
+# for osmesa
+apt-get update
+apt-get install python-opengl -y
+apt-get install libosmesa6 -y 
